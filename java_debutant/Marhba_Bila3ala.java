6839e471e465abb3195f5c23304163f78bb57440
youssef malki <ymalki20232@iesjoanramis.org>
2022-10-31 20:46:18 +0100

    premier prg fameux de Hello Word 2 commit formation java debutant marhba bikom

---
+++ java_debutant/Marhba_Bila3ala.java
@@ -0,0 +1,9 @@
+package java_debutant;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Marhba_Bila3ala {
+    public static void main(String[] args) {
+        System.out.println("- marhaban bikom f formation java");
+    }
+}
